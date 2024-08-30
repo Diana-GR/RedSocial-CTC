dfa4f2527cfd32d94b1d64df20447a7925766065
Diana-GR <guzman.dgr00@gmail.com>
2024-08-29 22:22:40 -0400

    .

--- posts/urls.py
+++ posts/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("login/", views.login, name="login"),
+    path("login/", views.session, name="login"),
     path("about-us/", views.aboutUs, name="about-us"),
     path("perfil/", views.perfil, name="perfil"),
     # path("", views.index, name = "index")
