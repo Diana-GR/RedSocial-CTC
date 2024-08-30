d701eb4811e6c4368745754aca1de513963f6d17
Diana-GR <guzman.dgr00@gmail.com>
2024-08-29 22:19:56 -0400

    methods implemented

--- posts/urls.py
+++ posts/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
-    # path("login/", views.session, name = "index")
-    # path("", views.index, name = "index")
-    # path("", views.index, name = "index")
+    path("", views.index, name="index"),
+    path("login/", views.login, name="login"),
+    path("about-us/", views.aboutUs, name="about-us"),
+    path("perfil/", views.perfil, name="perfil"),
     # path("", views.index, name = "index")
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,84 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User  # Utilizamos el modelo integrado User
+from django.contrib.auth import login, authenticate, logout
 
 
+# def index(request):
+#     return render(request, "index.html")
 # Create your views here.
 def index(request):
+    if request.user.is_authenticated:
+        return redirect("home")
     return render(request, "index.html")
+
+
+def signup(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+
+    if request.method == "GET":
+        return render(request, "signup.html", {"form": UserCreationForm})
+    else:
+        print(request.POST)
+        try:
+            if request.POST["password1"] == request.POST["password2"]:
+                user = User.objects.create_user(
+                    username=request.POST["username"],
+                    password=request.POST["password1"],
+                )
+                user.save()
+                login(request, user)
+                return redirect("home")
+            else:
+                return render(
+                    request,
+                    "signup.html",
+                    {"form": UserCreationForm, "error": "Las contraseñas no coinciden"},
+                )
+        except:
+            return render(
+                request,
+                "signup.html",
+                {"form": UserCreationForm, "error": "El usuario ya existe"},
+            )
+
+
+def session(request):
+    if request.user.is_authenticated:
+        return redirect("home")
+
+    if request.method == "GET":
+        return render(request, "login.html", {"form": AuthenticationForm})
+    else:
+        user = authenticate(
+            request,
+            username=request.POST["username"],
+            password=request.POST["password"],
+        )
+        if user is None:
+            return render(
+                request,
+                "login.html",
+                {
+                    "form": AuthenticationForm,
+                    "error": "El username o password es incorrecto",
+                },
+            )
+        login(request, user)
+        return redirect("home")
+
+
+def delete_session(request):
+    if request.user.is_authenticated:
+        logout(request)
+        return redirect("login")
+
+    return redirect("index")
+
+
+def home(request):
+    if request.user.is_authenticated:
+        return render(request, "home.html")
+    return redirect("index")
