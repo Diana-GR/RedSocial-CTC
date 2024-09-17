893ba381bb14215fbe9fb220320a38d05cb0a1ee
Claudia Aviles <zclaudiaaviles@gmail.com>
2024-09-17 11:10:10 -0400

    ultimos cambios

--- posts/forms.py
+++ posts/forms.py
@@ -3,27 +3,6 @@ from django.forms import ModelForm
 from .models import Task, Profile, Comentario, Post
 
 
-class TaskForm(ModelForm):
-    class Meta:
-        model = Task
-        fields = ["title", "description", "important"]
-        widgets = {
-            "title": forms.TextInput(attrs={"class": "form-control"}),
-            "description": forms.Textarea(attrs={"class": "form-control"}),
-            "important": forms.CheckboxInput(attrs={"class": "form-check-input"}),
-        }
-
-
-class ProfileForm(ModelForm):
-    class Meta:
-        model = Profile
-        fields = ["biography", "avatar"]
-        widgets = {
-            "biography": forms.Textarea(attrs={"class": "form-control bg-primary"}),
-            "avatar": forms.ClearableFileInput(attrs={"class": "form-control-file"}),
-        }
-
-
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
