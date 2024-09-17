29454bd3a12bec1cef740f226e20137696d6b76f
Claudia Aviles <zclaudiaaviles@gmail.com>
2024-09-17 16:42:40 -0400

    ultimos cambios

--- posts/views.py
+++ posts/views.py
@@ -259,7 +259,7 @@ def buscarUser(request):
 
 def create_post(request):
     if request.method == "POST":
-        form = PostForm(request.POST, request.Files)
+        form = PostForm(request.POST, request.FILES)
         if form.is_valid():
             post = form.save(commit=False)
             post.user = request.user
@@ -268,3 +268,25 @@ def create_post(request):
     else:
         form = PostForm()
     return render(request, "create_post.html", {"form": form})
+
+
+@login_required
+def edit_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id, user=request.user)
+    if request.method == "POST":
+        form = PostForm(request.POST, request.FILES, instance=post)
+        if form.is_valid():
+            form.save()
+            return redirect("home")
+    else:
+        form = PostForm(instance=post)
+    return render(request, "edit_post.html", {"form": form})
+
+
+@login_required
+def delete_post(request, post_id):
+    post = get_object_or_404(Post, id=post_id, user=request.user)
+    if request.method == "POST":
+        post.delete()
+        return redirect("home")
+    return render(request, "confirm_delete.html", {"post": post})
