d11873fb3b16510e62ee517eea68330622c040b8
Diana-GR <guzman.dgr00@gmail.com>
2024-09-19 20:43:29 -0400

    Finish

--- posts/views.py
+++ posts/views.py
@@ -203,27 +203,6 @@ def aboutus(request):
 
 
 @login_required
-def seguirUsers(request, user_id):
-    perfil_a_seguir = get_object_or_404(User, id=user_id)
-
-    if request.user == perfil_a_seguir:
-        print("Estás intentando seguirte a tí mismo.")
-        return redirect("profile")  # No permite que un usuario se siga a sí mismo
-
-    amistad, created = Amistad.objects.get_or_create(
-        usuario=request.user, amigo=perfil_a_seguir
-    )
-
-    if created:
-        print(f"{request.user.username} empezó a seguir a {perfil_a_seguir.username}.")
-    else:
-        amistad.delete()  # dejar de seguir
-        print(f"{request.user.username} dejó de seguir a {perfil_a_seguir.username}.")
-
-    return redirect("profile_otros", user_id=user_id)
-
-
-@login_required
 def seguirUser(request, user_id):
     perfil_a_seguir = get_object_or_404(User, id=user_id)
 
@@ -234,27 +213,15 @@ def seguirUser(request, user_id):
 
     # Verificar si ya sigue al usuario
     if perfil_a_seguir in request.user.seguir.all():
-        # Si ya lo sigue, eliminar la relación (dejar de seguir)
+        # Si ya lo sigue, deja de seguir
         request.user.seguir.remove(perfil_a_seguir)
         print(f"{request.user.username} dejó de seguir a {perfil_a_seguir.username}.")
     else:
-        # Si no lo sigue, añadir la relación (empezar a seguir)
+        # Si no lo sigue, empieza a seguir
         request.user.seguir.add(perfil_a_seguir)
         print(f"{request.user.username} empezó a seguir a {perfil_a_seguir.username}.")
 
     return redirect("profile_otros", user_id=user_id)
-
-
-def buscar(request):
-    query = request.GET.get("q")
-    if query:
-        users = User.objects.filter(
-            Q(username__icontains=query) | Q(profile__biography__icontains=query)
-        )
-    else:
-        users = User.objects.none()
-
-    return render(request, "buscador.html", {"users": users})
 
 
 def buscarUser(request):
